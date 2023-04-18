12efffae0c3b7b7b7d5eb318a68728f6d71b4eea
Ganiyu Basirat Olanike <nikkibrownie217@gmail.com>
2023-04-18 10:06:20 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import './App.css'
 import Hero from './components/Hero'
 import 'bootstrap/dist/css/bootstrap.css'
 import Hook from './components/Hook'
-import Fetch from './components/fetch'
+import { browserRouter, Routes, Route } from "react-router-dom"
+
+
+
 
 function App() {
-  let myDetails = {
-    myName: 'John Doe',
-    age: 30,
-    location: 'New York'
-  }
+  
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    {/* <Hook/> */}
-    {/* <Fetch/> */}
-    </>
+      <>
+      <Hook/>
+
+      
+      </>
   )
 }
-
+  
 export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './navbar'
+import Navbar from './Object'
 import Second from './second'
 import Hook from './Hook'
 
