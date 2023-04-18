d0dab5f916bcc033c9d6fe2484b1278fa247a41f
Ganiyu Basirat Olanike <nikkibrownie217@gmail.com>
2023-04-18 12:54:21 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hero from './components/Hero'
 import 'bootstrap/dist/css/bootstrap.css'
-import Hook from './components/Hook'
-import { browserRouter, Routes, Route } from "react-router-dom"
+import Fetch from './components/Fetch'
+// import { browserRouter, Routes, Route } from "react-router-dom"
 
 
 
@@ -16,9 +15,7 @@ function App() {
 
   return (
       <>
-      <Hook/>
-
-      
+        <Fetch/>
       </>
   )
 }
